df519567b0a200dea5aa449c8d6a0441fdcaa00e
Daniel da Silva <daniel@developmentseed.org>
2018-12-12 15:15:45 +0000

    Ram-tools: Include yarn

--- ram-tools/install.sh
+++ ram-tools/install.sh
@@ -3,12 +3,17 @@
 apt-get update
 apt-get install -y curl
 
-apt-get install -y build-essential git cmake pkg-config \
+apt-get install -y build-essential git cmake pkg-config apt-transport-https \
 libbz2-dev libstxxl-dev libstxxl1v5 libxml2-dev \
 libzip-dev libboost-all-dev lua5.2 liblua5.2-dev libtbb-dev libluabind-dev
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 \. "/root/.nvm/nvm.sh" && nvm install 6
+
+# Install yarn.
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get install -y --no-install-recommends yarn
 
 # Make node available for everyone
 # https://www.digitalocean.com/community/tutorials/how-to-install-node-js-with-nvm-node-version-manager-on-a-vps
